b88c857089a0ec49d2a1b75d905550f1bf915ba4
umairnadeem123 <nadeem.umair123@gmail.com>
2019-05-16 19:18:44 -0700

    Implement database seeding script

--- server/index.js
+++ server/index.js
@@ -5,7 +5,9 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/../client/public'));
-// app.get('/', (req, res) => {
-//     res.send('bruddda');
-// });
-app.listen(port);
+
+app.get('/stocks/:stock', (req, res) => {
+    res.send(req.params.stock);
+});
+
+app.listen(port, () => console.log(`Running on port ${port}`));
