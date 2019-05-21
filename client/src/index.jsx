de190d4369825424f9d55457476cf29b0aa6711f
umairnadeem123 <nadeem.umair123@gmail.com>
2019-05-21 15:01:38 -0700

    Remove React router from imports

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,5 @@
 import App from './components/App.jsx';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 ReactDOM.render(<App/>, document.getElementById('earnings-chart'));
