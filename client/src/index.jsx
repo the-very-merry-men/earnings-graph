b0faffb121803655c809ad5044f582f5bb3d5f6e
umairnadeem123 <nadeem.umair123@gmail.com>
2019-05-29 11:02:46 -0700

    Put public key in folder

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,6 @@
 import App from './components/App.jsx';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '../public/styles.css';
 
 ReactDOM.render(<App/>, document.getElementById('earnings-chart'));
--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,14 @@ module.exports = {
         query: {
           presets: ['react', 'es2015']
         }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: "file-loader"
       }
     ]
   }
